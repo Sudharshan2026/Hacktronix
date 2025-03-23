1bd6e3d70bebb21a5cef687c08c1f26182928001
Sudharshan2026 <bshanth0206@gmail.com>
2025-03-23 08:38:48 +0530

    done1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+
 import Hero from "./components/Hero";
-import Description from "./components/Description";
-import { TimelineSection } from "./components/TimeLineSection";
-import Rules from "./components/Rules";
-import OrganizersPage from "./components/OrganizersPage";
-import { FireParticles } from "./components/FireParticles";
-import CollegeMap from "./components/CollegeMap";
-import WhyJoinUs from "./components/WhyJoinUs";
-import Domains from "./components/Domains";
-import Results from "./components/Results";
-import Preloader from "./components/Preloader";
-import Prizes from "./components/Prizes";
+
 
 function ScrollToHashElement() {
   const location = useLocation();
@@ -46,33 +35,27 @@ function App() {
     }
   }, []);
 
-  if (loading) {
-    return <Preloader />;
-  }
+  // if (loading) {
+  //   return <Preloader />;
+  // }
 
   return (
     <Router>
       <ScrollToHashElement />
       <div className="flex flex-col min-h-screen">
-        <FireParticles />
+
         <main className="flex-grow">
-          <Header />
           <Routes>
             <Route path="/" element={
               <div>
                 <Hero />
-                <Description />
-                <Prizes />
-                <WhyJoinUs />
-                <TimelineSection />
-                <CollegeMap />
-                <Footer />
+
               </div>
             } />
-            <Route path="/Domain" element={<div><Domains /> <Footer /> </div>} />
+            {/* <Route path="/Domain" element={<div><Domains /> <Footer /> </div>} />
             <Route path="/team" element={<div><OrganizersPage /> <Footer /> </div>} />
             <Route path="/guidelines" element={<div><Rules /> <Footer /> </div>} />
-            <Route path="/results" element={<Results />} />
+            <Route path="/results" element={<Results />} /> */}
           </Routes>
         </main>
       </div>
